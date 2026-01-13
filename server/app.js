fd78fcccc786750d997e5637c6e90d08c8d216a1
Pablo Queiroz <pablo.hl2@gmail.com>
2026-01-13 16:13:20 +0000

    mongoose

--- server/app.js
+++ server/app.js
@@ -14,6 +14,12 @@ const app = express();
 
 const cohorts = require("./cohorts.json")
 const students = require("./students.json")
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mini-project")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
